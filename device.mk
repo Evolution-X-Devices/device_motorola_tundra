0d86eccc0d2e46b35606f10e3a235c4a517edff7
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-17 18:16:11 -0400

    tundra: Build MotCamera4 from prebuilt source
    Change-Id: I2505c20df05977a113643f26d0d293b355cb6c48
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -63,6 +63,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
 
+# Moto Camera 4
+TARGET_MOTCAMERA4 := tundra
+TARGET_USES_MOTCAMERA4 := true
+
+$(call inherit-product, vendor/motorola/MotCamera4-lahaina/motcamera4.mk)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
